da6f33244203dd4794d04384190dd27d7fc492f6
khoa-omega <nvkhoa05@gmail.com>
2024-11-28 21:26:14 +0700

    Update

--- blog-app/src/main/java/com/vti/blog_app/exception/ErrorHandler.java
+++ blog-app/src/main/java/com/vti/blog_app/exception/ErrorHandler.java
@@ -1,20 +1,105 @@
 package com.vti.blog_app.exception;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.ConstraintViolationException;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.TypeMismatchException;
+import org.springframework.http.*;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.io.IOException;
 import java.util.LinkedHashMap;
 
 @ControllerAdvice
-public class ErrorHandler extends ResponseEntityExceptionHandler {
+public class ErrorHandler extends ResponseEntityExceptionHandler
+        implements AuthenticationEntryPoint, AccessDeniedHandler {
+    @Override
+    public void commence(
+            HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException authException
+    ) throws IOException, ServletException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        var message = "Tài khoản hoặc mật khẩu không đúng";
+        var error = new ErrorResponse(message, null);
+        var out = response.getOutputStream();
+        new ObjectMapper().writeValue(out, error);
+    }
+
+    @Override
+    public void handle(
+            HttpServletRequest request, HttpServletResponse response,
+            AccessDeniedException accessDeniedException
+    ) throws IOException, ServletException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        var message = "Quyền truy cập bị từ chối";
+        var error = new ErrorResponse(message, null);
+        var out = response.getOutputStream();
+        new ObjectMapper().writeValue(out, error);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleException(Exception exception) {
+        var message = "Internal server error";
+        var response = new ErrorResponse(message, null);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleTypeMismatch(
+            TypeMismatchException exception,
+            HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        if (exception instanceof MethodArgumentTypeMismatchException e) {
+            var requiredType = e.getRequiredType();
+            var message = "Type mismatch";
+            var response = new ErrorResponse(message, null);
+            return new ResponseEntity<>(response, headers, status);
+        }
+        return super.handleTypeMismatch(exception, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(
+            MissingServletRequestParameterException exception,
+            HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        var message = "Missing request parameter";
+        var response = new ErrorResponse(message, null);
+        return new ResponseEntity<>(response, headers, status);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(
+            HttpMediaTypeNotSupportedException exception,
+            HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        var message = "Media type not supported";
+        var response = new ErrorResponse(message, null);
+        return new ResponseEntity<>(response, headers, status);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
+            HttpRequestMethodNotSupportedException exception,
+            HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        var message = "Method not supported";
+        var response = new ErrorResponse(message, null);
+        return new ResponseEntity<>(response, headers, status);
+    }
+
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
             MethodArgumentNotValidException ex,
